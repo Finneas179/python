3e2b7a0ea49a63dbc35d53da944738f0b1ce7def
Finneas <1758843145@qq.com>
2026-01-29 22:57:06 +0800

    第一次上传我的代码

---
+++ 上课/数据分析/matplotlib入门.py
@@ -0,0 +1,5 @@
+from matplotlib import pyplot as plt
+
+plt.plot([2,3,4,5,6,98])
+
+plt.show()
---
+++ 上课/数据分析/两条线.py
@@ -0,0 +1,4 @@
+from matplotlib import pyplot as plt
+plt.plot([2,3,4,5,6,98])
+plt.plot([1,4,2,3,7],[4,23,8,6,4])
+plt.show()
